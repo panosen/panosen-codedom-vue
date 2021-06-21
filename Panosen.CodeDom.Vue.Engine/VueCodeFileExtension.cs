aad92b4afcdd0f6a73674c0755cba4595d311f29
hc_zhang <hc_zhang@trip.com>
2021-06-21 19:04:47 +0800

    add VueCodeFileExtension.cs

---
+++ Panosen.CodeDom.Vue.Engine/VueCodeFileExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.CodeDom.Vue.Engine
+{
+    /// <summary>
+    /// VueCodeFileExtension
+    /// </summary>
+    public static class VueCodeFileExtension
+    {
+        /// <summary>
+        /// TransformText
+        /// </summary>
+        /// <param name="vueCodeFile"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static string TransformText(this VueCodeFile vueCodeFile, GenerateOptions options = null)
+        {
+            var builder = new StringBuilder();
+
+            new VueCodeFileEngine().GenerateCodeFile(vueCodeFile, builder, options);
+
+            return builder.ToString();
+        }
+    }
+}
